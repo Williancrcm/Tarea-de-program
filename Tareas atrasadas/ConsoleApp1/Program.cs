f533935ded22d390339b5b26d73f573a5967a24c
Williancrcm <87875119+Williancrcm@users.noreply.github.com>
2021-08-29 22:32:20 -0600

    Add files via upload

---
+++ Tareas atrasadas/ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        private static bool Menu()
+        {
+            Console.WriteLine("Seleccion la operacion a realizar: ");
+            Console.WriteLine("1. Registrar nuevo estudiante");
+            Console.WriteLine("1. Registrar nuevo estudiante");
+            Console.WriteLine("1. Registrar nuevo estudiante");
+            Console.WriteLine("1. Registrar nuevo estudiante");
+            Console.WriteLine("1. Registrar nuevo estudiante");
+            Console.Write("")
+        }
+    }
+}
